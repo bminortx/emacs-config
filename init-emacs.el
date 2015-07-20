8a9fa7bd8f858f1a222cfb95ee6571dd30c536df
Brandon Minor <brandon@replicalabs.com>
2015-07-20 09:43:30 -0600

    Desktop to my-env

--- init-emacs.el
+++ init-emacs.el
@@ -1,18 +1,22 @@
+;;; package --- Summary
+;;; Commentary:
+;;; init-emacs.el loads all other packages and configuration files,
+;;; then starts Emacs the way we want it.
+
+;;; Code:
 (let ((default-directory "~/.emacs.d/"))
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
 (server-start)
 
+;; basic initialization, (require) non-ELPA packages, etc.
 (require 'tramp)
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/")
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
-
-;; basic initialization, (require) non-ELPA packages, etc.
 (setq package-enable-at-startup nil) 
 (package-initialize)
-
 (require 'my-packages)
 (require 'my-appearance)
 (require 'my-moving)
@@ -27,26 +31,6 @@
 (require 'my-fun-packages)
 (require 'my-cpplint-fix)
 
-;;;;;;;;;;;;;;;;
-;; Desktop Saving and Loading functionality
-(provide 'desktop-autosave)
-(eval-when-compile
-  (require 'cl))
-(require 'saveplace)
-(setq-default save-place t)
-(require 'desktop)
-(setq desktop-dirname             "~/.emacs.d/"
-      desktop-base-file-name      ".emacs.desktop"
-      desktop-base-lock-name      "lock"
-      desktop-path                (list desktop-dirname))
-(desktop-save-mode 1) ;; Switch on desktop.el
-(desktop-load-default)
-(desktop-read)
-(defun desktop-autosave-save ()
-  (desktop-save desktop-dirname))
-(add-hook 'auto-save-hook 'desktop-autosave-save)
-
-;;;;;;;;;;;;;;;;
 (which-function-mode)
 (powerline-default-theme)
 (add-to-list 'default-frame-alist '(font . "Ubuntu Mono-9"))
--- my-env.el
+++ my-env.el
@@ -1,3 +1,12 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Settings for both the global (host) environment, and
+;;; the Emacs environment
+
+;;; Code:
+
+;;;;;;;;;;;;;;;;;
+;;; System Environment: 
 (defun set-exec-path-from-shell-PATH ()
   (let ((path-from-shell (replace-regexp-in-string
                           "[ \t\n]*$"
@@ -7,16 +16,34 @@
     (setq exec-path (split-string path-from-shell path-separator))))
 
 (if window-system (set-exec-path-from-shell-PATH))
-
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/git/bin"))
 (setq exec-path (append exec-path '("/usr/local/git/bin")))
-
 (setenv "PATH" (concat (getenv "PATH") ":/sw/bin"))
 (setq exec-path (append exec-path '("/sw/bin")))
-
-
 (setenv "PATH" (concat (getenv "PATH") ":/opt/local/bin"))
 (setq exec-path (append exec-path '("/opt/local/bin")))
+
+
+;;;;;;;;;;;;;;;;;
+;;; Emacs Environment:
+
+;;; Desktop Saving and Loading functionality
+(provide 'desktop-autosave)
+(eval-when-compile
+  (require 'cl))
+(require 'saveplace)
+(setq-default save-place t)
+(require 'desktop)
+(setq desktop-dirname             "~/.emacs.d/"
+      desktop-base-file-name      ".emacs.desktop"
+      desktop-base-lock-name      "lock"
+      desktop-path                (list desktop-dirname))
+(desktop-save-mode 1) ;; Switch on desktop.el
+(desktop-load-default)
+(desktop-read)
+(defun desktop-autosave-save ()
+  (desktop-save desktop-dirname))
+(add-hook 'auto-save-hook 'desktop-autosave-save)
 
 (provide 'my-env)
 ;;; my-env.el ends here
